1541db99811d10da85bc5d5578825872bdfac1b6
zakiaazhari <zakiaulia861@gmail.com>
2021-05-06 10:28:19 +0700

    tugas pertemuan 11

---
+++ pertemuan11/functions.php
@@ -0,0 +1,95 @@
+<?php
+
+function koneksi()
+{
+  return mysqli_connect("localhost", "root", "", "pw_203040006");
+}
+
+
+function query($query)
+{
+  $conn = Koneksi();
+
+  $result = mysqli_query($conn, $query);
+
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
+
+function tambah($data)
+{
+  $conn = Koneksi();
+
+  $nama = htmlspecialchars($data['nama']);
+  $nrp  = htmlspecialchars($data['nrp']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "INSERT INTO
+              mahasiswa
+            VALUE
+            (null, '$nama', '$nrp', '$email', '$jurusan', '$gambar');
+            ";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  echo mysqli_error($conn);
+  return mysqli_affected_rows($conn);
+}
+
+function hapus($id)
+{
+  $conn = Koneksi();
+  mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id") or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+  $conn = Koneksi();
+
+  $id = $data['id'];
+  $nama = htmlspecialchars($data['nama']);
+  $nrp  = htmlspecialchars($data['nrp']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "UPDATE mahasiswa SET
+              nama = '$nama',
+              nrp = '$nrp',
+              email = '$email',
+              jurusan = '$jurusan',
+              gambar = '$gambar'
+              WHERE id = $id";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  echo mysqli_error($conn);
+  return mysqli_affected_rows($conn);
+}
+
+function cari($keyword)
+{
+  $conn = koneksi();
+
+  $query = "SELECT *FROM mahasiswa 
+              WHERE 
+              nama LIKE '%$keyword%' OR
+              nrp LIKE '%$keyword%'
+              ";
+
+  $result = mysqli_query($conn, $query);
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
---
+++ pertemuan11/index.php
@@ -0,0 +1,70 @@
+<?php
+require 'functions.php';
+$mahasiwa = query("SELECT * FROM mahasiswa");
+
+// ketika cari tombol klik
+if (isset($_POST['cari'])) {
+  $mahasiswa = cari($_POST['keyword']);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <style>
+    img {
+      height: 140px;
+      ;
+    }
+  </style>
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <a href="tambah.php">Tambah Daftar Mahasiswa</a>
+  <br><br>
+
+  <form action="" method="POST">
+    <input type="text" name="keyword" size="40" placeholder="masukan keyword pencarian.." autocomplete="off" autofocus>
+    <button type="submit">Cari!</button>
+  </form>
+
+  <br>
+  <table border="1" cellspacing="0" cellpadding="10">
+    <tr>
+      <th>Id</th>
+      <th>Gambar</th>
+      <th>Nama</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php if (empty($mahasiwa)) : ?>
+      <tr>
+        <td colspan="4">
+          <p style="color : red; font-style: italic;">data mahasiswa tidak ditemukan!</p>
+        </td>
+      </tr>
+    <?php endif; ?>
+    <?php $i = 1; ?>
+    <?php foreach ($mahasiwa as $m) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $m['gambar']; ?>" width=""></td>
+        <td><?= $m['nama']; ?></td>
+        <td>
+          <a href="detail.php?id=<?= $m['id']; ?>">lihat detail</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
